f0565db6f8d60a4f2699085a25dc26fc63319ef6
Sankar boro <sankar.boro@yahoo.com>
2023-07-12 08:46:53 +0530

    reanimated working

--- src/ImageComponent.tsx
+++ src/ImageComponent.tsx
@@ -2,12 +2,33 @@ import React from 'react';
 import {
   Text,
   View,
+  Button,
+  StyleSheet
 } from 'react-native';
+import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
 
 export default function ImageComponent(): JSX.Element {
+  const offset = useSharedValue(0);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    'worklet' // this one is Important
+    return {
+      transform: [{ translateX: offset.value * 255 }],
+    };
+  }, []);
+
   return (
-    <View>
-      <Text>Image Component</Text>
-    </View>
+    <>
+      <Animated.View style={[styles.box, animatedStyles]} />
+      <Button onPress={() => (offset.value = Math.random())} title="Move" />
+    </>
   );
 }
+
+
+const styles = StyleSheet.create({
+  box: {
+      padding: 50,
+      backgroundColor: "red"
+  },
+});
