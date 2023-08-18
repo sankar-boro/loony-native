3f5edaf246308560b3dae7c0be15436e7324d1d2
Sankar boro <sankar.boro@yahoo.com>
2023-08-18 10:04:50 +0530

    Loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
@@ -12,6 +13,7 @@ import UpdatePasswordPage from './components/UpdatePasswordPage';
 import {NAMES} from './utils/Constants';
 import {PaperProvider} from 'react-native-paper';
 import {MD3LightTheme as DefaultTheme} from 'react-native-paper';
+import {Loading} from './components/Loading';
 
 const Stack = createNativeStackNavigator();
 const theme = {
@@ -23,6 +25,67 @@ const theme = {
   },
 };
 
+function ViewContainer({password}: any): JSX.Element {
+  if (password.load === false) {
+    return <Loading />;
+  }
+
+  if (password.load === true) {
+    if (password.hasPassword === false) {
+      return (
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen
+            name={NAMES.CREATE_APP_PASSWORD_PAGE}
+            component={AppPassword}
+            options={{title: 'App Password'}}
+          />
+        </Stack.Navigator>
+      );
+    } else if (password.hasPassword === true && password.auth === false) {
+      return (
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen
+            name={NAMES.LOGIN_PAGE}
+            component={LoginPage}
+            options={{title: 'Login'}}
+          />
+        </Stack.Navigator>
+      );
+    } else {
+      return (
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen
+            name={NAMES.HOME_PAGE}
+            component={HomeScreen}
+            options={{title: 'Loony'}}
+          />
+          <Stack.Screen
+            name={NAMES.ENCRYPT_PASSWORD_PAGE}
+            component={EncryptPasswordPage}
+            options={{title: 'Encrypt Password'}}
+          />
+          <Stack.Screen
+            name={NAMES.VIEW_ENCRYPTED_PASSWORDS_PAGE}
+            component={ViewEncryptedPasswordsPage}
+            options={{title: 'View All Password', headerShown: true}}
+          />
+          <Stack.Screen
+            name={NAMES.UPDATE_PASSWORDS_PAGE}
+            component={UpdatePasswordPage}
+            options={{title: 'Update Password', headerShown: true}}
+          />
+        </Stack.Navigator>
+      );
+    }
+  }
+
+  return (
+    <View>
+      <Text>Failed to load component</Text>
+    </View>
+  );
+}
+
 function Navigation(): JSX.Element {
   return (
     <NavigationContainer>
@@ -30,47 +93,7 @@ function Navigation(): JSX.Element {
         <ServiceProvider>
           <AppContext.Consumer>
             {({password}) => {
-              if (password.auth === false) {
-                return (
-                  <Stack.Navigator screenOptions={{headerShown: false}}>
-                    <Stack.Screen
-                      name={NAMES.LOGIN_PAGE}
-                      component={LoginPage}
-                      options={{title: 'Login'}}
-                    />
-                    <Stack.Screen
-                      name={NAMES.CREATE_APP_PASSWORD_PAGE}
-                      component={AppPassword}
-                      options={{title: 'App Password'}}
-                    />
-                  </Stack.Navigator>
-                );
-              } else {
-                return (
-                  <Stack.Navigator screenOptions={{headerShown: false}}>
-                    <Stack.Screen
-                      name={NAMES.HOME_PAGE}
-                      component={HomeScreen}
-                      options={{title: 'Loony'}}
-                    />
-                    <Stack.Screen
-                      name={NAMES.ENCRYPT_PASSWORD_PAGE}
-                      component={EncryptPasswordPage}
-                      options={{title: 'Encrypt Password'}}
-                    />
-                    <Stack.Screen
-                      name={NAMES.VIEW_ENCRYPTED_PASSWORDS_PAGE}
-                      component={ViewEncryptedPasswordsPage}
-                      options={{title: 'View All Password', headerShown: true}}
-                    />
-                    <Stack.Screen
-                      name={NAMES.UPDATE_PASSWORDS_PAGE}
-                      component={UpdatePasswordPage}
-                      options={{title: 'Update Password', headerShown: true}}
-                    />
-                  </Stack.Navigator>
-                );
-              }
+              return <ViewContainer password={password} />;
             }}
           </AppContext.Consumer>
         </ServiceProvider>
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export const Loading = () => {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <Text>Loading</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    alignItems: 'center',
+  },
+});
